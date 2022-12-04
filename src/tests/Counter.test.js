a639bb5368754071c0617024519d5a7099768197
cameron-keene <74378283+cameron-keene@users.noreply.github.com>
2022-12-04 12:16:45 -0500

    implement 100% unit testing coverage

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,35 @@
 // import necessary react testing library helpers here
 // import the Counter component here
+import { fireEvent, render, screen } from '@testing-library/react';
+import Counter from '../components/App';
 
 beforeEach(() => {
   // Render the Counter component here
+  render(<Counter />);
 })
 
 test('renders counter message', () => {
   // Complete the unit test below based on the objective in the line above
+  const counterMessage = screen.getByText(/Counter/i);
+  expect(counterMessage).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
   // Complete the unit test below based on the objective in the line above
+  const val = screen.getByTestId('count');
+  expect(val).toHaveTextContent(0);
 });
 
 test('clicking + increments the count', () => {
   // Complete the unit test below based on the objective in the line above
+  fireEvent.click(screen.getByText("+"));
+  const val = screen.getByTestId('count');
+  expect(val).toHaveTextContent(1);
 });
 
 test('clicking - decrements the count', () => {
   // Complete the unit test below based on the objective in the line above
+  fireEvent.click(screen.getByText("-"));
+  const val = screen.getByTestId('count');
+  expect(val).toHaveTextContent(-1);
 });
